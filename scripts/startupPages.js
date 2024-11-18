27278cd956b39763ac557e6b20ae35bd95ba62a9
sympsy <83234716+sympsy@users.noreply.github.com>
2024-11-18 14:18:44 -0500

    major updates

---
+++ scripts/startupPages.js
@@ -0,0 +1,73 @@
+const countdownLength = 5
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("startup");
+
+    let gameWrapper = document.getElementById("gameWrapper")
+    let gameplayWrapper = document.getElementById("gameplayWrapper")
+    let leaderboardWrapper = document.getElementById("leaderboardWrapper")
+
+    let titlePage = document.getElementById("titlePage")
+    let startButton = document.getElementById("startButton")
+    let instructionsButton = document.getElementById("instructionsButton")
+    let settingsButton = document.getElementById("settingsButton")
+
+    let instructionsPage = document.getElementById("instructionsPage")
+    let settingsPage = document.getElementById("settingsPage")
+    let okayButtons = document.getElementsByClassName("okayButton1")
+
+    console.log("startup complete");
+
+    for (let i = 0; i < okayButtons.length; i++) {
+        okayButtons[i].onclick = function() {
+            titlePage.style.display = "flex"
+            settingsPage.style.display = "none"
+            instructionsPage.style.display = "none"
+        }
+    }
+
+    function hideAllPages() {
+        titlePage.style.display = "none"
+        settingsPage.style.display = "none"
+        instructionsPage.style.display = "none"
+    }
+
+    startButton.onclick = function() {
+        hideAllPages()
+        gameplayWrapper.style.display = "flex"
+        leaderboardWrapper.style.display = "none"
+        runGame()
+    }
+
+    instructionsButton.onclick = function() {
+        hideAllPages()
+        instructionsPage.style.display = "flex"
+    }
+
+    settingsButton.onclick = function() {
+        hideAllPages()
+        settingsPage.style.display = "flex"
+    }
+});
+
+function runGame(){
+    let countdown = document.getElementById("countdown")
+    countdown.innerHTML = countdownLength
+    console.log("starting loop")
+    
+    let delay = 0;
+    
+    for (let i = countdownLength; i>0; i=i-1) {
+        console.log("oopng")
+        console.log(i)
+        setTimeout(() => {
+            countdown.innerHTML = i
+        }, delay);
+        delay += 1000;
+    }
+
+    setTimeout(() => {
+        countdown.innerHTML = "GO!"
+    }, delay);
+    delay += 1000
+}
